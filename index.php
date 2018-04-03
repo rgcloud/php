e3b381be49dc7d55d0d56c79628f9ec566f633b9
rgcloud <37324173+rgcloud@users.noreply.github.com>
2018-04-03 12:11:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<center> <h1> Welcome To our openshift DevOps(CI/CD) project</h1> </center>
+
+<?php
+
+echo "Hi omkar :-) This is a new code";
+
+?>
+
+</body>
+</html>
